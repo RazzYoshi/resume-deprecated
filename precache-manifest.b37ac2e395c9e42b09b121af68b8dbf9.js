269d1ea891d2ef0817d724dc6c6c34128b686685
Raymond Chung <rchung@latticeautomation.com>
2019-09-11 23:08:55 -0400

    Updates

---
+++ precache-manifest.b37ac2e395c9e42b09b121af68b8dbf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "023749a94339734e5706ae02955ebd64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a35ed0e8c7fde4e0e46",
+    "url": "/static/css/main.ce480664.chunk.css"
+  },
+  {
+    "revision": "1aa6e4aff2c69a31fa3d",
+    "url": "/static/js/2.b4a8f07f.chunk.js"
+  },
+  {
+    "revision": "9a35ed0e8c7fde4e0e46",
+    "url": "/static/js/main.13eda962.chunk.js"
+  },
+  {
+    "revision": "8f91b97b19b07d70c85d",
+    "url": "/static/js/runtime~main.7359d637.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.12ef78ce3acb828ad694c3b7b374dd71.js"
+  "/precache-manifest.b37ac2e395c9e42b09b121af68b8dbf9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
